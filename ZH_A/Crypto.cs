a4f8b3bcd9474d93a83ece9117912877e9ae5da2
Betti <Betti@DESKTOP-EU7PIGR>
2024-02-11 20:38:28 +0100

    Add project files.

---
+++ ZH_A/Crypto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZH_A
+{
+    internal class Crypto
+    {
+        string date;
+        string name;
+        float open;
+        float close;
+
+        public Crypto(string date, string name, float open, float close)
+        {
+            this.date = date;
+            this.name = name;
+            this.open = open;
+            this.close = close;
+        }
+
+        public string Name { get { return name; } }
+        public string Date { get { return date; } }
+        public float Open { get { return open; } }
+        public float Close { get { return close; } }
+    }
+}
---
+++ ZH_A/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.ComponentModel;
+
+namespace ZH_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("crypto.csv");
+            Crypto[] cryptos = new Crypto[lines.Length-1];
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] parts = lines[i].Split(";");
+
+                cryptos[i - 1] = new Crypto(parts[0], parts[1], float.Parse(parts[2]), float.Parse(parts[3]));
+            }
+
+
+
+            Console.WriteLine("Tell me a year!");
+            string targetYear = Console.ReadLine();
+            Console.WriteLine("Tell me a month!");
+            string targetMonth = Console.ReadLine();
+            List<float> Bitcoin = new List<float>();
+            List<float> Ethereum = new List<float>();
+            List<float> Tether = new List<float>();
+            foreach (Crypto line in cryptos)
+            {
+                if(line.Date.Substring(0,4) ==targetYear && line.Date.Substring(5,2) == targetMonth)
+                {
+                    if(line.Name == "Bitcoin")
+                    {
+                        Bitcoin.Add(line.Close - line.Open);
+                    }
+                    if (line.Name == "Ethereum")
+                    {
+                        Ethereum.Add(line.Close - line.Open);
+                    }
+                    if (line.Name == "Tether")
+                    {
+                        Tether.Add( line.Close - line.Open);
+                    }
+                }
+
+            }
+            for(int i = 0; i<Bitcoin.Count;i++)
+            {
+                Console.WriteLine(Bitcoin[i]);
+            }
+            for (int i = 0; i < Ethereum.Count; i++)
+            {
+                Console.WriteLine(Ethereum[i]);
+            }
+            for (int i = 0; i < Tether.Count; i++)
+            {
+                Console.WriteLine(Tether[i]);
+            }
+
+
+
+            Console.WriteLine("Tell me a crypo name!");
+            string targetName = Console.ReadLine();
+            float allOpen = 0;
+            float allClose = 0;
+            float counter = 0;
+            foreach(Crypto line in cryptos)
+            {
+                if(line.Name == targetName)
+                {
+                    allOpen += line.Open;
+                    allClose += line.Close;
+                    counter++;
+                }
+            }
+            Console.WriteLine(allOpen / counter);
+            Console.WriteLine(allClose / counter);
+
+
+
+            int max = 0;
+            string maxName = "";
+            foreach (Crypto line in cryptos)
+            {
+                if ((line.Close - line.Open) / line.Open > max)
+                {
+                    maxName = line.Name;
+                }
+            }
+            Console.WriteLine(maxName);
+            Console.Read();
+        }
+    }
+}
